9a61a4b928c76e78252ef3381fa56a6123b972fc
Gilberto Junior <junior.siilva182@gmail.com>
2021-02-27 22:28:24 -0300

    added initial divs

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,20 @@ const Header = () => {
       <div className="header__logo">
         <img src={TeslaLogo} alt="Tesla Logo" />
       </div>
+
+      <div className="header__center">
+        <p>Model S</p>
+        <p>Model 3</p>
+        <p>Model X</p>
+        <p>Model Y</p>
+        <p>Solar Roof</p>
+        <p>Solar Panel</p>
+      </div>
+
+      <div className="header__right">
+        <p>Shop</p>
+        <p>Tesla Account</p>
+      </div>
     </div>
   );
 };
